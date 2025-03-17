d46e4b8ae8af1454d00813e6190a6352aaacd004
nordeim <nordeimkuahsziyp@outlook.com>
2025-03-17 21:10:45 +0800

    Add files via upload

---
+++ sample_smolagents.py
@@ -0,0 +1,6 @@
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+
+model = HfApiModel()
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
+
+agent.run("what is Group Relative Policy Optimization for Reinforcement Learning?")
